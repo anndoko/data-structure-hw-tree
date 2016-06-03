c00b3858e78aae015ae52e8ceb6ee19f0717c66d
Anndo <handcuffanndo@gmail.com>
2016-06-03 22:29:35 +0800

    Fix: improve code style and add new comments

--- oopTree.cpp
+++ oopTree.cpp
@@ -11,74 +11,102 @@ void BinaryTree::inorder() {inorder(root);}
 void BinaryTree::inorder(BinaryTreeNode *CurrentNode) {
     if(CurrentNode){
         inorder(CurrentNode->LeftChild);
-        cout<<CurrentNode->data<<" ";
+        cout << CurrentNode->data << " ";
         inorder(CurrentNode->RightChild);
     }
 }
 
-void CompleteBinaryTree::Insert(int d){tree[++n]=d;}
-
+void CompleteBinaryTree::Insert(int d){tree[++n] = d;}
 void CompleteBinaryTree::inorder(){
-    cout<<"Inorder Traversal"<<endl;
+    cout << "Inorder Traversal" <<endl;
     inorder(1);
     cout<<endl<<endl;
 }
 void CompleteBinaryTree::inorder(int current) {
-    if(current<=n) {
+    if(current <= n) {
         inorder(2*current);
         cout <<tree[current]<<" ";
         inorder(2*current+1);
     }
 };
 
-
-void MaxHeap::Insert(const int key){
-    //cout << "Do void MaxHeap::Insert(const int key), insert: " << key << endl;
-    int i;
+Boolean BST::Insert(const int d){   // insert int d into the BST
     
-    if(n == MaxSize){HeapFull(); return;}
-    n++;
-    for(i = n; 1 ;){
-        if(i == 1) break;
-        if(key <= heap[i/2]) break;
-        heap[i] = heap[i/2];
-        i=i/2;
-    }
-    heap[i] = key;
-}
-
-// Delete from the max heap
-int* MaxHeap::DeleteMax(int& x){
-    //cout << "\nDo int* MaxHeap::DeleteMax(int& x), delete: ";
-    if(n==0) {HeapEmpty(); return 0;}
-    x = heap[1]; int k = heap[n]; n--;
+    /* this line is for testing purpose
+    cout << "Do Boolean BST::Insert(const int d), insert: " << endl;
+    */
     
-    int i=1, j=2;
+    BinaryTreeNode *p = root;   // q is parent of p
+    BinaryTreeNode *q = 0;
     
-    for(i;j<=n;){
-        if(j<n) if(heap[j] < heap[j+1]) j++;
-        if(k>heap[j])break;
-        heap[i] = heap[j];
-        i=j; j*=2;
-    }
-    heap[i]=k;
-    return &x;
-}
-
-Boolean BST::Insert(const int d){
-    //cout << "Do Boolean BST::Insert(const int d), insert: " << endl;
-    BinaryTreeNode *p = root; BinaryTreeNode *q = 0;
     while(p){
-        q=p;
-        if(d == p->data) return FALSE;
+        q = p;
+        
+        if(d == p->data) return FALSE;  // d is already existing
+        
         if(d < p->data) p = p->LeftChild;
         else p = p->RightChild;
     }
+    
+    //  perform insertion
     p = new BinaryTreeNode;
-    p->LeftChild = p->RightChild = 0; p->data = d;
+    p->LeftChild = p->RightChild = 0;
+    p->data = d;
     if(!root) root = p;
     else if(d < q->data) q->LeftChild = p;
     else q->RightChild =p;
     
     return TRUE;
 }
+
+void MaxHeap::Insert(const int key){    // insert int key into the max heap
+    
+    /* this line is for testing purpose
+    cout << "Do void MaxHeap::Insert(const int key), insert: " << key << endl;
+    */
+    
+    int i;
+    
+    if(n == MaxSize){
+        HeapFull();
+        return;
+    }
+    
+    n++;
+    
+    for(i = n; 1 ;){
+        if(i == 1) break;   // at root
+        if(key <= heap[i/2]) break;
+        heap[i] = heap[i/2];    // move from parent to i
+        i = i/2;
+    }
+    heap[i] = key;
+}
+
+int* MaxHeap::DeleteMax(int& x){ // remove x from the max heap
+    
+    /* this line is for testing purpose
+    cout << "\nDo int* MaxHeap::DeleteMax(int& x), delete: ";
+    */
+    
+    if(n == 0) {
+        HeapEmpty();
+        return 0;
+    }
+    
+    x = heap[1]; int k = heap[n]; n--;
+    
+    int i = 1;  // i: vacant position
+    int j = 2;  // j: larger child
+    
+    for(i; j <= n;){
+        if(j < n) if(heap[j] < heap[j+1]) j++;
+        if(k > heap[j]) break;
+        heap[i] = heap[j]; // move the child up
+        i=j; j*=2; // move i and j downward
+    }
+    heap[i]=k;
+    return &x;
+}
+
+
